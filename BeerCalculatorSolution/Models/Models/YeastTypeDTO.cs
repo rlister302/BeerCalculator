aef0c61f7265965e3fd4ef941ffe3a4d39348b31
Ryan-PC\Ryan <rlister302@gmail.com>
2016-11-12 14:40:10 -0500

    Created yeast type dto

---
+++ BeerCalculatorSolution/Models/Models/YeastTypeDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Models
+{
+    public class YeastTypeDTO
+    {
+        public int YeastTypeID { get; set; }
+        public string YeastName { get; set; }
+        public int LowAttenuationRate { get; set; }
+        public int HighAttenuationRate { get; set; }
+        public int LowTemperatureRange { get; set; }
+        public int HighTemperatureRange { get; set; }
+
+    }
+}
