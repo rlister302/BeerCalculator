d240877d1f199f96f340e7914d3f7389212eb060
Nicolas Shapland <nshapland88@gmail.com>
2016-11-12 15:26:59 -0500

    added log4net to test app

--- BeerCalculatorSolution/ConsoleApplication1/Program.cs
+++ BeerCalculatorSolution/ConsoleApplication1/Program.cs
@@ -4,14 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DataAccessLayer;
-
+using log4net;
 namespace ConsoleApplication1
 {
     class Program
     {
+        private static readonly log4net.ILog log =
+            log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         static void Main(string[] args)
         {
-            GetHops();
+            try
+            {
+                GetHops();
+            }
+            catch (Exception e)
+            {
+                log.Error(e.Message);
+                log.Error(e.InnerException);
+            }
             Console.WriteLine("Please Press a Key");
             Console.ReadKey();
         }
@@ -19,9 +29,21 @@ namespace ConsoleApplication1
         {
             using (var ctx = new DataAccessLayer.BeerCalculatorEntities())
             {
-                var queryResult = ctx.Recipes.Where(r => r.RecipeName.Equals("Summer Lager", StringComparison.OrdinalIgnoreCase)).ToList();
+                try
+                {
+                    var hopsList = ctx.Hops.Where
+                        (h => h.RecipeID == ctx.Recipes.Where(r => r.RecipeName.Equals("Summer Lager", StringComparison.OrdinalIgnoreCase)).FirstOrDefault().RecipeID);
 
-                Console.WriteLine(queryResult.Count);
+                    DataAccessLayer.Grain myObj = null;
+
+                    myObj.Amount = 10;
+
+                    Console.WriteLine(hopsList.Count());
+                }
+                catch
+                {
+                    throw;
+                }
             }
         }
     }
