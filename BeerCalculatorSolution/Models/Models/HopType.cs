147300055230e9ac88c1fe8ef91c44c5e0841d4a
Ryan-PC\Ryan <rlister302@gmail.com>
2016-11-12 14:16:14 -0500

    Commit model HopType

---
+++ BeerCalculatorSolution/Models/Models/HopType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Models
+{
+    public class HopType
+    {
+        public int HopTypeID { get; set; }
+        public string HopName { get; set; }
+        public string FlavorNotes { get; set; }
+    }
+}
