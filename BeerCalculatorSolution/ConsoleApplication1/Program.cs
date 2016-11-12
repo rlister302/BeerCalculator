1fcc602677546c6dbff6d3a940f1a5764c13d029
Nicolas Shapland <nshapland88@gmail.com>
2016-11-12 14:57:12 -0500

    added a test app

---
+++ BeerCalculatorSolution/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetHops();
+            Console.ReadKey();
+        }
+        static void GetHops()
+        {
+            using (var ctx = new DataAccessLayer.BeerCalculatorEntities())
+            {
+                var queryResult = ctx.Recipes.Where(r => r.RecipeName.Equals("Summer Lager", StringComparison.OrdinalIgnoreCase)).ToList();
+
+                Console.WriteLine(queryResult.Count);
+            }
+        }
+    }
+}
