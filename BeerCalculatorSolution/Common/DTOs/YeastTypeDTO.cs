c6abdde1829c8a0a35a68f90e02ae95bd365db9c
Ryan-PC\Ryan <rlister302@gmail.com>
2016-11-20 12:59:07 -0500

    Alright, I think I actually fixed it this time.Removed conflicts from DTOs

--- BeerCalculatorSolution/Common/DTOs/YeastTypeDTO.cs
+++ BeerCalculatorSolution/Common/DTOs/YeastTypeDTO.cs
@@ -7,11 +7,8 @@ using Common.Abstract;
 
 namespace Common.DTOs
 {
-<<<<<<< HEAD:BeerCalculatorSolution/Common/DTOs/YeastTypeDTO.cs
+
     public class YeastTypeDTO : ModelBase
-=======
-    public class YeastTypeDTO
->>>>>>> 4d4d2b14b50f429e520bea005f5107de2f1c95da:BeerCalculatorSolution/Models/Models/YeastTypeDTO.cs
     {
         public int YeastTypeID { get; set; }
         public string YeastName { get; set; }
